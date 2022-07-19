44185b191080c5bcb6a74e1ef4aae3592ebb96ac
Ihor Oryshchenko <igor89.or@gmail.com>
2022-07-19 16:56:38 +0300

    Script for build & deploy, static serving and switched indentation to 2

--- server/src/server.js
+++ server/src/server.js
@@ -8,11 +8,11 @@ const PORT = process.env.PORT || 8000;
 const server = http.createServer(app);
 
 async function startServer() {
-    await loadPlanetsData();
+  await loadPlanetsData();
 
-    server.listen(PORT, () => {
-        console.log(`Listening on port ${PORT}...`);
-    });
+  server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+  });
 }
 
 startServer();

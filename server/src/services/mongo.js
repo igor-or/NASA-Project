bad1b3dba4146d9f1630c8e6d014fa92d1bc1426
Ihor Oryshchenko <igor89.or@gmail.com>
2022-07-21 21:59:15 +0300

    Dotenv for configuration and secrets

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
+require('dotenv').config();
+
 // Update below to match your own MongoDB connection string.
-const MONGO_URL =
-  'mongodb+srv://nasa-api:6KftecMJMHQDhJ0h@nasacluster.fppw9.mongodb.net/nasa?retryWrites=true&w=majority';
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once('open', () => {
   console.log('MongoDB connection ready!');
